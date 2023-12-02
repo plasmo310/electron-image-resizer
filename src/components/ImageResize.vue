e8f720f1a3974c0216ba81cf11f6cc7ae3cab8bc
plasma <plasma@plasmanoMacBook-Pro.local>
2023-12-02 16:25:14 +0900

    create UI mock.

---
+++ src/components/ImageResize.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="container">
+    <div class="container-item image-area">
+      <!-- TODO 読み込んだ画像を表示する -->
+      <!-- <img class="image-item" src="../assets/img/test_image.JPG" /> -->
+      <div class="image-drop-box">画像をドラッグ＆ドロップしてください。</div>
+    </div>
+    <div class="container-item size-info-area">
+      <input class="size-input-value-item" type="number" min="0" placeholder="width (px)" />
+      <div class="size-input-value-between">x</div>
+      <input class="size-input-value-item" type="number" min="0" placeholder="height (px)" />
+    </div>
+    <div class="container-item output-file-area">
+      <input class="output-file-value" type="text" />
+      <button class="output-file-button">出力</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input {
+  border-radius: 4px;
+}
+.container {
+  text-align: center;
+  display: flex;
+  flex-flow: column;
+}
+.container-item {
+  margin-bottom: 24px;
+}
+/** 画像エリア */
+.image-area {
+  display: table;
+  height: 230px;
+  width: 100%;
+  padding: 12px 0px;
+}
+.image-item {
+  max-width: 320px;
+  max-height: 280px;
+}
+.image-drop-box {
+  color: #777;
+  border: 10px dashed #aaa;
+  padding: 0px 12px;
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+  border-radius: 8px;
+}
+/** サイズ情報 */
+.size-info-area {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  height: 32px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.size-input-value-between {
+  width: 80px;
+}
+.size-input-value-item {
+  width: 80%;
+  height: 100%;
+  text-align: right;
+}
+/** 出力パスエリア */
+.output-file-area {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 32px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.output-file-value {
+  height: 100%;
+  width: 100%;
+}
+.output-file-button {
+  width: 80px;
+  height: 120%;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 12px;
+}
+</style>
--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,31 +1,31 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+    height: 480,
+    width: 640,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
-  });
+  })
 
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(path.join(__dirname, '../index.html'))
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createWindow()
 
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
